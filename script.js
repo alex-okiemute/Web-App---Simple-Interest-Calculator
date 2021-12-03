1d269348d1ee0f3885cfaa8e3323c643f1dc5199
alex-okiemute <94883893+alex-okiemute@users.noreply.github.com>
2021-12-03 12:33:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function compute()
     var interest = principal * years * rate /100; //variable interest
     var year = new Date().getFullYear()+parseInt(years);
     var amount = interest + principal; //Variable to calculate Total amount recieved
-        
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>" 
+       
+    document.getElementById("result").innerHTML="If you deposit '<mark>'"+principal+"'</mark>',\<br\>at an interest rate of '<mark>'"+rate+"'</mark>'%\<br\>You will receive an amount of '<mark>'"+amount+"'</mark>',\<br\>in the year '<mark>'"+year+"'</mark>'\<br\>" 
 }
 
 //Function that reads the value of the range slider and displays dynatically
@@ -21,8 +21,8 @@ function updateRate()
 //Function to ensure Principal values are not 0 or negative
 function validateAmount() {
     var principal = document.getElementById("principal").value;
-    var moreThenZero = parseInt(principal) > 0;
-    if (!moreThenZero) {
+    var moreThanZero = parseInt(principal) > 0;
+    if (!moreThanZero) {
         alert("Enter a positive number please");
         document.getElementById("principal").focus();
     }
